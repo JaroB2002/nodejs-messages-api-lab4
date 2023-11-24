6e5724977a8d8a453318205718da7ef9d75f19c7
JaroB2002 <116017813+JaroB2002@users.noreply.github.com>
2023-11-24 20:35:49 +0100

    Feat: Express router for message CRUD operations

--- index.js
+++ index.js
@@ -7,6 +7,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const cors = require('cors');
 
 // require routes
 const messagesRouter = require('./routes/api/v1/messages');
@@ -17,6 +18,7 @@ const messagesRouter = require('./routes/api/v1/messages');
 
 // json body parser
 app.use(express.json());
+app.use(cors());
 app.use("/api/v1/messages", messagesRouter);
 
 app.listen(port, () => {
